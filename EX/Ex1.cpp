93bd50d4bf655de52a174fa385b0eaccfbd3e6ac
naininut <naininut@gmail.com>
2022-08-06 14:41:57 +0700

    first commit

---
+++ EX/Ex1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    printf("Thanat Varasajja\n");
+    int w,h;
+    scanf("%d %d",&w,&h);
+    printf("%d\n%d",w*h,2*(w+h));
+    return 0;
+}
+//git remote add origin https://github.com/naininut/POSN.git
+
+//git remote add origin https://naininut@github.com/naininut/POSN.git
